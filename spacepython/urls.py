2a7b5f0e5a29be187308e13c52d55330376815bb
EngineerPaul <kpashka@mail.ru>
2026-08-13 07:09:29 +0400

    prefix works

--- spacepython/urls.py
+++ spacepython/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
     # path('auth/', include('djoser.urls.authtoken')),
     path('auth/', include('djoser.urls.jwt')),
 ]
+
+urlpatterns = [
+    path('extra/calendar/', include(urlpatterns)),
+]
